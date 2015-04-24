12aefaf9dfae9ed05537818706739170902606bd
Nick Grygoriev <nick.grygoriev@gmail.com>
2015-04-24 21:28:12 +0300

    Add HomeWork on delegates lection

---
+++ HW_Delegates/Task1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Task1
+{
+    public delegate int Operation(int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation add = (x, y) => { return x + y; };
+            Operation sub = (x, y) => { return x - y; };
+            Operation mul = (x, y) => { return x * y; };
+            Operation div = (x, y) =>
+            {
+                if (y == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                else
+                {
+                    return x / y;
+                }
+            };
+
+            Console.WriteLine("Input first number");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Input second number");
+            int b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input operation (add/sub/mul/div)");
+            string action = Console.ReadLine();
+
+            int res;
+            switch (action)
+            {
+                case "add":
+                    res = add(a, b);
+                    Console.WriteLine("{0} + {1} = {2}", a, b, res);
+                    break;
+                case "sub":
+                    res = sub(a, b);
+                    Console.WriteLine("{0} - {1} = {2}", a, b, res);
+                    break;
+                case "mul":
+                    res = mul(a, b);
+                    Console.WriteLine("{0} * {1} = {2}", a, b, res);
+                    break;
+                case "div":
+                    try
+                    {
+                        res = div(a, b);
+                        Console.WriteLine("{0} / {1} = {2}", a, b, res);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Division by zero error!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Unknown operation");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW_Delegates/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task2
+{
+    public delegate int RandomDelegate();
+    public delegate int DelegateArray(RandomDelegate[] randArray);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int delegatesNum = 10;
+            Random r = new Random();
+            RandomDelegate[] rand = new RandomDelegate[delegatesNum];
+
+            for (int i = 0; i < delegatesNum; i++)
+            {
+                rand[i] = delegate()
+                {
+                    return r.Next(100);
+                };
+            }
+
+            
+            DelegateArray delArray = delegate(RandomDelegate[] randArray)
+            {
+                int sum = 0;
+
+                for (int i = 0; i < delegatesNum; i++)
+                {
+                    sum += randArray[i]();
+                }
+
+                return sum/delegatesNum;
+            };
+
+            Console.WriteLine("Average value: {0}", delArray(rand));
+            Console.ReadKey();
+        }
+    }
+}
